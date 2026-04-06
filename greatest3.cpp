7038add0fbefecca676bb596f21e4d1b06819667
notJishnu <jishnuguha121@gmail.com>
2026-04-06 19:13:28 +0530

    Add program to determine the greatest of three numbers

---
+++ greatest3.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+	int a=5,b=10,c=7;
+	if(a>b && a>c){
+		printf("A is greater");
+	}else if(b>a && b>c){
+		printf("B is greater");
+	}else{
+		printf("C is greater");
+	}
+	return 0;
+}
