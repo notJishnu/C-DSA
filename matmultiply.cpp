7b96ba2eed0ebde27aae3cbc712d647335d0ef4e
notJishnu <jishnuguha121@gmail.com>
2026-04-13 12:13:47 +0530

    Add program for matrix multiplication

---
+++ matmultiply.cpp
@@ -0,0 +1,58 @@
+#include <stdio.h>
+
+int main() {
+    int r1, c1, r2, c2;
+
+    // Input matrix sizes
+    printf("Enter rows and columns of Matrix A: ");
+    scanf("%d %d", &r1, &c1);
+
+    printf("Enter rows and columns of Matrix B: ");
+    scanf("%d %d", &r2, &c2);
+
+    // Check multiplication condition
+    if (c1 != r2) {
+        printf("Matrix multiplication not possible!\n");
+        return 0;
+    }
+
+    int A[r1][c1], B[r2][c2], C[r1][c2];
+
+    // Input Matrix A
+    printf("\nEnter elements of Matrix A:\n");
+    for (int i = 0; i < r1; i++) {
+        for (int j = 0; j < c1; j++) {
+            scanf("%d", &A[i][j]);
+        }
+    }
+
+    // Input Matrix B
+    printf("\nEnter elements of Matrix B:\n");
+    for (int i = 0; i < r2; i++) {
+        for (int j = 0; j < c2; j++) {
+            scanf("%d", &B[i][j]);
+        }
+    }
+
+    // Matrix multiplication logic
+    for (int i = 0; i < r1; i++) {
+        for (int j = 0; j < c2; j++) {
+            C[i][j] = 0;
+            for (int k = 0; k < c1; k++) {
+                C[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
+
+    // Display Result
+    printf("\nResultant Matrix (A × B):\n");
+    for (int i = 0; i < r1; i++) {
+        for (int j = 0; j < c2; j++) {
+            printf("%d ", C[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+
