227288f7269973f908a06d027eb2f8aafc401d95
notJishnu <jishnuguha121@gmail.com>
2026-04-20 19:10:50 +0530

    Add program to generate Fibonacci series

---
+++ fibonacci.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+void fibonacci(int n) {
+    int a = 0, b = 1, c, i;
+
+    if (n <= 0) {
+        printf("Invalid Input");
+        return;
+    }
+
+    printf("Fibonacci Series: ");
+
+    for(i = 1; i <= n; i++) {
+        printf("%d ", a);
+        c = a + b;
+        a = b;
+        b = c;
+    }
+}
+
+int main() {
+    int terms;
+
+    printf("Enter number of terms: ");
+    scanf("%d", &terms);
+
+    fibonacci(terms);
+
+    return 0;
+}
+
